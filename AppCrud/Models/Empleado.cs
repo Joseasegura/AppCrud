e3bb3131e6dec759fca280640392fbff635cf0d8
7james40 <sgjames470@gmail.com>
2024-10-04 21:51:28 -0500

    Agregar archivos de proyecto.

---
+++ AppCrud/Models/Empleado.cs
@@ -0,0 +1,12 @@
+﻿namespace AppCrud.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public String Nombre { get; set; }
+        public string Correo { get; set; }
+        public DateOnly FechaContrato { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
